730e2d41b87c212f53471ac304100d8f5131a839
Eka Yulianita Widyanti <eywidyanti@gmail.com>
2023-06-21 08:33:57 +0700

    firebase

---
+++ lib/login_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../sign_in.dart';
+import '../first_screen.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const FlutterLogo(size: 150),
+              const SizedBox(height: 50),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlinedButton(
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const FirstScreen();
+                },
+              ),
+            );
+          }
+        });
+      },
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+        side: const BorderSide(color: Colors.grey),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            //Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
